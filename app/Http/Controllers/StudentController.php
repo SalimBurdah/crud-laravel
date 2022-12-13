9efba8d2fc6c8dbc2ea81d8a84c6e820699cd04e
SalimBurdah <salimburdah@gmail.com>
2022-12-13 16:23:16 +0700

    modified studentcontroller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,84 +1,70 @@
 <?php
-
 namespace App\Http\Controllers;
-
+  
+use App\Models\Student;
 use Illuminate\Http\Request;
-
+  
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        $students = Student::latest()->paginate(5);
+      
+        return view('students.index',compact('students'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
     public function create()
     {
-        //
+        return view('students.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+  
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'id' => 'required',
+            'name' => 'required',
+            'nim' => 'required',
+            'alamat' => 'required'
+        ]);
+      
+        Student::create($request->all());
+       
+        return redirect()->route('students.index')
+                        ->with('success','Student created successfully.');
+    }
+  
+    public function show(Student $student)
+    {
+        return view('students.show',compact('student'));
+    }
+  
+    public function edit(Student $student)
+    {
+        return view('students.edit',compact('student'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function update(Request $request, Student $student)
     {
-        //
+        $request->validate([
+            'id' => 'required',
+            'name' => 'required',
+            'nim' => 'required',
+            'alamat' => 'required',
+        ]);
+      
+        $student->update($request->all());
+      
+        return redirect()->route('students.index')
+                        ->with('success','Student updated successfully');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function destroy(Student $student)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $student->delete();
+       
+        return redirect()->route('students.index')
+                        ->with('success','Student deleted successfully');
     }
 }
